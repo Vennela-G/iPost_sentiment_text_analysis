e81d2b4d6bf169096ae67368372ba62ed0c4859c
Vennela-G <34793850+Vennela-G@users.noreply.github.com>
2021-04-24 18:40:21 -0400

    Deprecated old directory

--- iPost/AppDefinitions.h
+++
@@ -1,7 +0,0 @@
-#ifndef APPDEF_HDR_H
-#define APPDEF_HDR_H
-
-enum class SensitivityPrefEnum { MILD = 1, MODERATE, HIGH };
-enum class AccountStatusEnum { ACTIVE = 1, INACTIVE, DISABLED };
-
-#endif
